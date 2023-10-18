8e7f114cbe073b9b083a384195987bd45d87304f
ShunMiura <shun13andcooki3.1999@gmail.com>
2023-10-19 02:51:10 +0900

    三浦）猫が自動で動くプログラムを作成中です。現在は猪突猛進します

---
+++ Assets/Scenes/miura/NekoFront.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NekoFront : MonoBehaviour
+{
+    //
+    GameObject frontPanel;
+    [SerializeField]private bool walkFront;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // スタート時、NekoFrontは見えなくなる
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        walkFront = false;
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {// 猫の前のパネルを取得する
+    // パネルのタグをパネルにした
+        if (other.tag == "Panel")
+        {
+            bool tmp = other.GetComponent<Tile_select>().IsWalk;
+            walkFront = tmp; // 前が歩けるかどうかを更新
+            frontPanel = other.gameObject;
+        }
+
+    }
+
+    //  前のパネルが通れるか通れないかのみを返す変数
+    public bool GetWalkFront()
+    {
+        return walkFront;
+    }
+
+    //  前のパネルのオブジェクトを返すす変数
+    public GameObject GetFrontPanel()
+    {
+        return frontPanel;
+    }
+}
