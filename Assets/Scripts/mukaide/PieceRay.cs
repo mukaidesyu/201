08e89c24cac1e2721d1c0bc2da934ed748c17019
syu_mukaide <mukaide.syu@gmail.com>
2023-10-19 19:45:41 +0900

    向出)ピース選びなおせるよ

--- Assets/Scripts/mukaide/PieceRay.cs
+++ Assets/Scripts/mukaide/PieceRay.cs
@@ -4,30 +4,27 @@ using UnityEngine;
 
 public class PieceRay : MonoBehaviour
 {
+    public bool moveflag = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
- 
-    }
-
-    private void FixedUpdate()
-    {
-        Debug.DrawRay(gameObject.transform.position, Vector3.forward, Color.red, 10.0f);
 
         RaycastHit hit;
-        if (Physics.Raycast(gameObject.transform.position, Vector3.forward, out hit, 10.0f))
+        if (Physics.Raycast(this.transform.position, Vector3.forward, out hit, 10.0f))
         {
-            Debug.Log("あたってる");
-          
-            // 赤色に変更する
+            // 緑色に変更する
             hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.green;
+            //Debug.Log("あたってる");
 
         }
+
     }
+
 }
--- Assets/Scripts/mukaide/Tilemanager.cs
+++ Assets/Scripts/mukaide/Tilemanager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class Tilemanager : MonoBehaviour
 {
+    public bool walkflag = false;
+    private int TileNo = 0;
+
     // Start is called before the first frame update
     void Start()
     {
       gameObject.GetComponent<Renderer>().material.color = Color.red;
+        TileNo++;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if(walkflag == true)
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
 
+    public void WalkFlag()
+    {
+        walkflag = true;
     }
 }
