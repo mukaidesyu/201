952e6f7188ff8fd82406c0c4277f0288955c38be
Kenrinboy <wken0827@gmail.com>
2023-11-23 01:40:13 +0900

    渡辺）道でけた！

--- Assets/Scripts/miura/Tile_Material.cs
+++ Assets/Scripts/miura/Tile_Material.cs
@@ -14,15 +14,24 @@ public class Tile_Material : MonoBehaviour
         Right,
         Warm,
         Side,
+        RightUp,
+        DownRight,
+        TRight,
+        LeftUp,
+        Tup,
+        LeftDown,
+        TLeft,
+        TDown,
+        Cross,
     };
 
     Material[] tmp;
     TileStatus status;
 
-    [SerializeField]Tilemanager UpPanel;
-    [SerializeField]Tilemanager RightPanel;
-    [SerializeField]Tilemanager LeftPanel;
-    [SerializeField]Tilemanager DownPanel;
+    [SerializeField]bool UpPanel;
+    [SerializeField]bool RightPanel;
+    [SerializeField]bool LeftPanel;
+    [SerializeField]bool DownPanel;
 
     [SerializeField]  private int id;
 
@@ -48,27 +57,116 @@ public class Tile_Material : MonoBehaviour
         if (isWalk)
         {
             status = TileStatus.Up;
-            // 上のパネルを取得
-            if (id > 0)
+           
+            int cols = GameObject.Find("Tile").GetComponent<tile>()._cols;//列
+            int rows = GameObject.Find("Tile").GetComponent<tile>()._rows;//行
+            int road = 0;
+            /*Debug.Log("通った");*/
+            
+            if ((id + 1) % cols == 0)//上の例外処理
             {
-               // /*Debug.Log("通った");*/
-               // UpPanel = GameObject.Find("panel" + (id + 1) + "(Clone)").GetComponent<Tilemanager>();
-               // if()
-               // if (UpPanel.PutWalkFlag() == true)
-               // {
-               //     status = TileStatus.Warm;
-               // }
-               // DownPanel = GameObject.Find("panel" + (id - 1) + "(Clone)").GetComponent<Tilemanager>();
-               //
-               // int cols = GameObject.Find("Tile").GetComponent<tile>()._cols;
-               // RightPanel = GameObject.Find("panel" + (id + cols) + "(Clone)").GetComponent<Tilemanager>();
-               //
-               // LeftPanel = GameObject.Find("panel" + (id + -cols) + "(Clone)").GetComponent<Tilemanager>();
+                UpPanel = false;
             }
+            else
+            {
+                UpPanel = GameObject.Find("panel" + (id + 1) + "(Clone)").GetComponent<Tilemanager>().PutWalkFlag(); // 上のパネルを取得
+                if (UpPanel == true)
+                { road += 1; }
+            }
+
+            if (cols * (rows - 1) <= id)//右の例外処理
+            {
+                RightPanel = false;
+            }
+            else
+            {
+                RightPanel = GameObject.Find("panel" + (id + cols) + "(Clone)").GetComponent<Tilemanager>().PutWalkFlag();  // 右のパネルを取得
+                if (RightPanel == true)
+                { road += 10; }
+            }
+
+            if (id % cols == 0)//下の例外処理
+            {
+                DownPanel = false;
+            }
+            else
+            {
+                DownPanel = GameObject.Find("panel" + (id - 1) + "(Clone)").GetComponent<Tilemanager>().PutWalkFlag();  // 下のパネルを取得
+                if (DownPanel == true)
+                { road += 100; }
+            }
+
+            if (cols > id)//左の例外処理
+            {
+                LeftPanel = false;
+            }
+            else
+            {
+                LeftPanel = GameObject.Find("panel" + (id + -cols) + "(Clone)").GetComponent<Tilemanager>().PutWalkFlag(); // 左のパネルを取得
+                if (LeftPanel == true)
+                { road += 1000; }
+            }
+            
+            //左下右上の順
+            switch (road)
+            {
+                case 0:
+                    status = TileStatus.Grass;
+                    break;
+                case 1://上
+                    status = TileStatus.Up;
+                    break;
+                case 10://右
+                    status = TileStatus.Right;
+                    break;
+                case 11://右上
+                    status = TileStatus.RightUp;
+                    break;
+                case 100://下
+                    status = TileStatus.Down;
+                    break;
+                case 101://下上
+                    status = TileStatus.Warm;    
+                    break;
+                case 110://下右
+                    status = TileStatus.DownRight;
+                    break;
+                case 111://下右上
+                    status = TileStatus.TRight;
+                    break;
+                case 1000://左
+                    status = TileStatus.Left;
+                    break;
+                case 1001://左上
+                    status = TileStatus.LeftUp;
+                    break;
+                case 1010://左右
+                    status = TileStatus.Side;
+                    break;
+                case 1011://左右上
+                    status = TileStatus.Tup;
+                    break;
+                case 1100://左下
+                    status = TileStatus.LeftDown;
+                    break;
+                case 1101://左下上
+                    status = TileStatus.TLeft;
+                    break;
+                case 1110://左下右 
+                    status = TileStatus.TDown;
+                    break;
+                case 1111://左下右上 
+                    status = TileStatus.Cross;
+                    break;
+            }
+
         }
         else
         {
             status = TileStatus.Grass;
         }
+
+        
+
     }
 }
