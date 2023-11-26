c60392f2b3783dba2cd519b0f305abdf8649c53c
ShunMiura <shun13andcooki3.1999@gmail.com>
2023-11-26 17:57:30 +0900

    三浦）タイルの状況と連動して、タイルにアイテムが表示されるようになりました。

--- Assets/Scripts/miura/ItemSprite.cs
+++ Assets/Scripts/miura/ItemSprite.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class ItemSprite : MonoBehaviour
 {
-    Sprite[] sprite;
+    SpriteRenderer spriteRenderer;
+    public Sprite[] sprite;
+    Sprite now;
     EventStatus EventSta;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        now = sprite[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // ó·äOçXêVîªíË
+        if (now != sprite[(int)EventSta])
+        {
+            now = sprite[(int)EventSta];
+
+            switch (EventSta)
+            {
+                case EventStatus.None:
+                    spriteRenderer.sprite = now;
+                    break;
+                case EventStatus.Juel:
+                    Debug.Log("osu");
+                    spriteRenderer.sprite = now;
+
+                    break;
+                case EventStatus.Juel_Got:
+                    spriteRenderer.sprite = now;
+                    break;
+            }
+        }
+    }
+
+    public void SetEventSta(EventStatus status)
+    {
+        EventSta = status;
     }
 }
