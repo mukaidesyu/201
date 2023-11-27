143ef22e3a8f83bc3dd50b2c631dec265d9b6960
ShunMiura <shun13andcooki3.1999@gmail.com>
2023-11-28 00:47:46 +0900

    三浦）池絵導入！凄松原！！

--- Assets/Scripts/miura/Terrain.cs
+++ Assets/Scripts/miura/Terrain.cs
@@ -7,8 +7,7 @@ public class Terrain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //オブジェクトの色を赤に変更する
-        GetComponent<Renderer>().material.color = Color.red;
+                
     }
 
     // Update is called once per frame
