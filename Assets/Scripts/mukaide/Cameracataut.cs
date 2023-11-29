9d5ab3cfb21998862e2930b025acc6be48545ada
syu_mukaide <mukaide.syu@gmail.com>
2023-11-29 23:31:11 +0900

    向出）スカイボックス変更

---
+++ Assets/Scripts/mukaide/Cameracataut.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameracataut : MonoBehaviour
+{
+    //プレイヤーを変数に格納
+    public GameObject Player;
+
+    //回転させるスピード
+    public float rotateSpeed = 100.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //回転させる角度
+        float angle = -1 * rotateSpeed * Time.deltaTime;
+
+        //プレイヤー位置情報
+        Vector3 playerPos = Player.transform.position;
+
+        //カメラを回転させる
+        transform.RotateAround(playerPos, Vector3.up, angle);
+    }
+}
