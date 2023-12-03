bc35e037a199be7e54ba4af53ffb62ae5eee7615
syu_mukaide <mukaide.syu@gmail.com>
2023-12-04 00:54:30 +0900

    向出）パーティクル

---
+++ Assets/Scripts/mukaide/StartButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+
+    Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GameObject.Find("Start_Button").GetComponent<Button>();
+        //ƒ{ƒ^ƒ“‚ª‘I‘ğ‚³‚ê‚½ó‘Ô‚É‚È‚é
+        button.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
