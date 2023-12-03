3b51ffb35ffd4b7748577678f0accba9c343f6c9
syu_mukaide <mukaide.syu@gmail.com>
2023-12-04 03:23:45 +0900

    向出）クリアシーンきれいにした

---
+++ Assets/Scripts/mukaide/clearbutton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class clearbutton : MonoBehaviour
+{
+    Button button;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GameObject.Find("button1").GetComponent<Button>();
+        //ƒ{ƒ^ƒ“‚ª‘I‘ğ‚³‚ê‚½ó‘Ô‚É‚È‚é
+        button.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
