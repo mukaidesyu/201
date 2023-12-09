9e50816a2eb4fe0d32a00a6b247df4b6cb103625
ShunMiura <shun13andcooki3.1999@gmail.com>
2023-12-10 02:35:14 +0900

    三浦）巨大化演出でけた！

---
+++ Assets/Scripts/mukaide/NekoFade.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NekoFade : MonoBehaviour
+{
+    public float ChangeTime;
+    float time;
+
+    enum FadeState
+    {
+        None = 0,
+        FadeIn,
+        FadeOut,
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartNekoFade()
+    {
+
+    }
+}
