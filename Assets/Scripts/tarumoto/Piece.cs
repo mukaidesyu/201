bd42a68ba231d81bd35baf8265ab900e0362079c
syu_mukaide <mukaide.syu@gmail.com>
2023-12-11 01:27:08 +0900

    向出）赤くなるのをすこし修正

--- Assets/Scripts/tarumoto/Piece.cs
+++ Assets/Scripts/tarumoto/Piece.cs
@@ -22,22 +22,18 @@ public class Piece : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown("joystick button 0"))
+        int i = 0;
+        eflag = false;
+        for (i = 0; i < this.transform.childCount; i++)
         {
-            int i = 0;
-            eflag = false;
-            for (i = 0; i < this.transform.childCount; i++)
+            if (pc[i].GetComponent<PieceRay>().pfl() == true)
             {
-                if (pc[i].GetComponent<PieceRay>().pfl() == true)
-                {
-                    pflag = true;
-                }
-                if (pc[i].GetComponent<PieceRay>().Ev() == EventStatus.Ike || pc[i].GetComponent<PieceRay>().Ev() == EventStatus.Rock || pc[i].GetComponent<PieceRay>().Ps() == PanelStatus.Nothing)
-                {
-                    eflag = true;
-                }
+                pflag = true;
             }
-
+            if (pc[i].GetComponent<PieceRay>().Ev() == EventStatus.Ike || pc[i].GetComponent<PieceRay>().Ev() == EventStatus.Rock || pc[i].GetComponent<PieceRay>().Ps() == PanelStatus.Nothing)
+            {
+                eflag = true;
+            }
         }
 
     }
