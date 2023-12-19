27fda7004c5cf43660b65654a5d3cf3d32d5f3f8
syu_mukaide <mukaide.syu@gmail.com>
2023-12-20 00:13:45 +0900

    向出）クリア画面でボタン連打してもシーン変化しないようにしました

--- Assets/Scripts/mukaide/clearbutton.cs
+++ Assets/Scripts/mukaide/clearbutton.cs
@@ -5,19 +5,33 @@ using UnityEngine.UI;
 
 public class clearbutton : MonoBehaviour
 {
-   [SerializeField] Button button;
+   [SerializeField] Button button1;
+    [SerializeField] Button button2;
+
+    private float time = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        button = GameObject.Find("nextbutton").GetComponent<Button>();
+ 
+        time = 5;
+        button1 = GameObject.Find("nextbutton").GetComponent<Button>();
+        button1.GetComponent<Button>().interactable = false;
+        button2 = GameObject.Find("titlebutton").GetComponent<Button>();
+        button2.GetComponent<Button>().interactable = false;
         //É{É^ÉìÇ™ëIëÇ≥ÇÍÇΩèÛë‘Ç…Ç»ÇÈ
-        button.Select();
+        button1.Select();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time -= Time.deltaTime;
+        if(time <= 0)
+        {
+            button1.GetComponent<Button>().interactable = true;
+            button2.GetComponent<Button>().interactable = true;
+        }
+
     }
 }
