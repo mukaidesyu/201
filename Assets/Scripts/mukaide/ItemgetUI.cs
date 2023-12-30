de111573052c69eb23b529b79ca32aa7e8d2cd71
syu_mukaide <mukaide.syu@gmail.com>
2023-12-30 20:23:38 +0900

    向出）アイテムのカットイン演出修正

--- Assets/Scripts/mukaide/ItemgetUI.cs
+++ Assets/Scripts/mukaide/ItemgetUI.cs
@@ -9,28 +9,52 @@ public class ItemgetUI : MonoBehaviour
     public bool isItemGet;
     [SerializeField]GameObject ItemGetPanel;
     GetItemneko get;
+    RectTransform RectTransform_get;
+    private float scale = 0;
+    public Itemgetneko itemgetneko;
 
     // Start is called before the first frame update
     void Start()
     {
         isItemGet = false;
         ItemGetPanel = GameObject.Find("buck");
+        itemgetneko = GameObject.Find("Itemgetneko").GetComponent<Itemgetneko>();
+        RectTransform_get = ItemGetPanel.gameObject.GetComponent<RectTransform>();
         ItemGetPanel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(ItemGetPanel.activeSelf == true &&itemgetneko.Nekofinish() == false)
+        {
+            scale += 0.1f;
+            if(scale >= 1)
+            {
+                scale = 1.0f;
+            }
+            RectTransform_get.localScale = new Vector2(1.0f, scale);
+        }
 
+        if(itemgetneko.Nekofinish() == true)
+        {
+            Debug.Log("aaaaaaaaaa");
+            scale -= 0.1f;
+            if (scale <= 0)
+            {
+                scale = 0.0f;
+            }
+            RectTransform_get.localScale = new Vector2(1.0f, scale);
+        }
 
     }
 
     public void SwitchItemGet()
     {
-
         Time.timeScale = 1;
+        scale = 0;
+        RectTransform_get.localScale = new Vector2(1.0f, scale);
         ItemGetPanel.SetActive(false);
-
     }
 
     public void SwitchItemGetStart(int no)
