3142fc788a8a2bc5181a56ee727b6ab5f5c2795c
ShunMiura <shun13andcooki3.1999@gmail.com>
2024-01-06 02:58:38 +0900

    三浦）とちゅ

--- Assets/Scenes/miura/tutorialScripts/TutorialData.cs
+++ Assets/Scenes/miura/tutorialScripts/TutorialData.cs
@@ -29,11 +29,14 @@ public class TutorialData : MonoBehaviour
     GameObject Tutorial2Canvas;
     GameObject Tutorial3Canvas;
     GameObject Tutorial4Canvas;
+    GameObject Tutorial5Canvas;
 
     TalkDataScript talkData;
     [SerializeField] Scenarios scenarios;
 
     bool BOOL; // 汎用
+
+    GameObject Player;
 
     // Start is called before the first frame update
     void Start()
@@ -43,10 +46,12 @@ public class TutorialData : MonoBehaviour
         Tutorial2Canvas = GameObject.Find("TalkCanvas2");
         Tutorial3Canvas = GameObject.Find("TalkCanvas3");
         Tutorial4Canvas = GameObject.Find("TalkCanvas4");
+        Tutorial5Canvas = GameObject.Find("TalkCanvas5");
         talkCanvas.SetActive(true);
         Tutorial2Canvas.SetActive(false);
         Tutorial3Canvas.SetActive(false);
         Tutorial4Canvas.SetActive(false);
+        Tutorial5Canvas.SetActive(false);
         riria = GameObject.Find("riria");
         kuro = GameObject.Find("kuro");
         Waku = GameObject.Find("Waku");
@@ -55,6 +60,8 @@ public class TutorialData : MonoBehaviour
         scenarios = GetComponent<Scenarios>();
 
         BOOL = false;
+
+        Player = GameObject.Find("player");
     }
 
     // Update is called once per frame
@@ -72,6 +79,29 @@ public class TutorialData : MonoBehaviour
             case TutorialState.Play3:
                 Invoke("TutorialNext", 2.0f);
                 BOOL = true;
+                break;
+            case TutorialState.Play4:
+                GameObject guide = GameObject.Find("tutorialGuide");
+
+                float dis =  Vector3.Distance(Player.transform.position,guide.transform.position);
+
+                if (dis <= 0.2f)
+                {
+                    BOOL = true;
+                    TutorialNext();
+                }
+                break;
+            case TutorialState.Play5:
+                if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.DownArrow))
+                {
+                    BOOL = true;
+                    TutorialBack();
+                }
+
+                
+                //BOOL = true;
+
+
                 break;
         }
     }
@@ -100,6 +130,45 @@ public class TutorialData : MonoBehaviour
             case TutorialState.Play4:
                 Tutorial3Canvas.SetActive(false);
                 Tutorial4Canvas.SetActive(true);
+                break;
+            case TutorialState.Play5:
+                Tutorial4Canvas.SetActive(false);
+                Tutorial5Canvas.SetActive(true);
+                break;
+        }
+
+        BOOL = false;
+    }
+
+    public void TutorialBack()
+    {
+        if (state == TutorialState.Play1)return;
+
+        state = (TutorialState)((int)state - 1);
+        // ここで次の状態をセット
+        switch (state)
+        {
+            case TutorialState.Play1:
+                riria.SetActive(false);
+                kuro.SetActive(false);
+                NameWaku.SetActive(false);
+                talkData.SetScenario(scenarios.scenario02);
+                break;
+            case TutorialState.Play2:
+                talkCanvas.SetActive(false);
+                Tutorial2Canvas.SetActive(true);
+                break;
+            case TutorialState.Play3:
+                Tutorial2Canvas.SetActive(false);
+                Tutorial3Canvas.SetActive(true);
+                break;
+            case TutorialState.Play4:
+                Tutorial3Canvas.SetActive(false);
+                Tutorial4Canvas.SetActive(true);
+                break;
+            case TutorialState.Play5:
+                Tutorial4Canvas.SetActive(false);
+                Tutorial5Canvas.SetActive(true);
                 break;
         }
 
