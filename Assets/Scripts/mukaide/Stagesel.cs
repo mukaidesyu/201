bd0e0562285896c85d4d335dc3835a66ff7f9627
syu_mukaide <mukaide.syu@gmail.com>
2024-01-06 02:57:47 +0900

    向出）ステージセレクト

--- Assets/Scripts/mukaide/Stagesel.cs
+++ Assets/Scripts/mukaide/Stagesel.cs
@@ -20,6 +20,8 @@ public class Stagesel : MonoBehaviour
 
     GameObject focus;
 
+    RectTransform mappin;
+
     AudioSource audio;
 
     // Start is called before the first frame update
@@ -35,6 +37,8 @@ public class Stagesel : MonoBehaviour
         materials = GameObject.Find("materialname").GetComponent<Image>();
         level.sprite = texture[0];
 
+        mappin = GameObject.Find("mappin").GetComponent<RectTransform>();
+
         audio = GetComponent<AudioSource>();
 
         focus = EventSystem.current.currentSelectedGameObject;
@@ -43,6 +47,7 @@ public class Stagesel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (eventsistem.currentSelectedGameObject.name != ("タイトルへ"))
         {
             tmp.text = eventsistem.currentSelectedGameObject.name;
@@ -52,17 +57,25 @@ public class Stagesel : MonoBehaviour
         {
             level.sprite = texture[0];
             materials.sprite = texture[3];
+            mappin.position = new Vector2(eventsistem.currentSelectedGameObject.transform.position.x + 8, eventsistem.currentSelectedGameObject.transform.position.y - 2);
         }
         if (tmp.text == "ヒルメ森林")
         {
             level.sprite = texture[1];
             materials.sprite = texture[4];
+            mappin.position = new Vector2(eventsistem.currentSelectedGameObject.transform.position.x + 8, eventsistem.currentSelectedGameObject.transform.position.y - 2);
         }
         if (tmp.text == "バン山脈")
         {
             level.sprite = texture[2];
             materials.sprite = texture[5];
+            mappin.position = new Vector2(eventsistem.currentSelectedGameObject.transform.position.x + 8, eventsistem.currentSelectedGameObject.transform.position.y - 2);
         }
+        if(eventsistem.currentSelectedGameObject.name == ("タイトルへ"))
+        {
+            mappin.position = new Vector2(eventsistem.currentSelectedGameObject.transform.position.x - 18, eventsistem.currentSelectedGameObject.transform.position.y);
+        }
+
         if (focus != EventSystem.current.currentSelectedGameObject)
         {
             audio.PlayOneShot(audio.clip);
