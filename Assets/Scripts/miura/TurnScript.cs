aa0a0c2bc5f70dff081fc3980b330b25cd2a6ccd
syu_mukaide <mukaide.syu@gmail.com>
2024-01-14 00:44:40 +0900

    no message

--- Assets/Scripts/miura/TurnScript.cs
+++ Assets/Scripts/miura/TurnScript.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+
 public class TurnScript : MonoBehaviour
 {
     // åoâﬂÉ^Å[Éì
     int turn = 0;
+    int faceturn;
     TextMeshProUGUI tmp;
+    Live2D.Cubism.Framework.Expression.CubismExpressionController face;
 
     // Start is called before the first frame update
     void Start()
     {
         turn = 0;
+        faceturn = 0;
         tmp = this.GetComponent<TextMeshProUGUI>();
+        face = GameObject.Find("ririachan2").GetComponent<Live2D.Cubism.Framework.Expression.CubismExpressionController>();
     }
 
     // Update is called once per frame
@@ -25,6 +30,12 @@ public class TurnScript : MonoBehaviour
     public void TurnPlus()
     {
         turn++;
+        faceturn++;
+        if (faceturn == 10)
+        {
+            faceturn = 0;
+            face.FaceChange(2);
+        }
     }
 
     public void SetTurn(int set)
