cd302bee60467ed43d04ba58424ad010ec32944a
ShunMiura <shun13andcooki3.1999@gmail.com>
2024-01-16 01:54:02 +0900

    三浦）押し込みでカメラが正面に向きます

--- Assets/Scenes/okano/Neko_Camera.cs
+++ Assets/Scenes/okano/Neko_Camera.cs
@@ -7,11 +7,12 @@ public class Neko_Camera : MonoBehaviour
     //回転させるスピード
     public float rotateSpeed = 3.0f;
     Vector3 playerPos;
+    bool startAngle = false;
 
     // Use this for initialization
     void Start()
     {
-
+        startAngle = false;
     }
 
     // Update is called once per frame
@@ -21,14 +22,33 @@ public class Neko_Camera : MonoBehaviour
         playerPos = this.transform.position;
 
         if (Input.GetKey(KeyCode.O) || Input.GetAxis("Horizontal2") > 0.5f)
-        {           
+        {
+            startAngle = false;
             //カメラを回転させる
             transform.RotateAround(playerPos, Vector3.up, rotateSpeed);
         }
         else if (Input.GetKey(KeyCode.U) || Input.GetAxis("Horizontal2") < -0.5f)
         {
+            startAngle = false;
             //カメラを回転させる
             transform.RotateAround(playerPos, Vector3.up, -rotateSpeed);
         }
+
+
+        if(Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown("joystick button 9"))
+        {
+            startAngle = true;
+        }
+
+        if (startAngle)
+        {
+            CameraFoward();
+        }
+    }
+
+    void CameraFoward()
+    {
+        float angle = Mathf.LerpAngle(transform.eulerAngles.y, Vector3.forward.y, 0.3f);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, angle, transform.eulerAngles.z);
     }
 }
